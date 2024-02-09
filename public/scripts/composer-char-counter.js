0cd622c6cf6cd779291e14e0c901f5ab8c02faf4
Jan Edmark Arieta <edarieta09@gmail.com>
2024-02-09 17:42:56 +0000

    characters left updates, turns red on negative values

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  console.log("It's ready")
+  $('#tweet-text').on('input', function() {
+    var charactersLeft = 140 - $(this).val().length;
+    $('.counter').text(charactersLeft);
+    if (charactersLeft < 0) {
+      $('.counter').css('color', 'red');
+    } else {
+      $('.counter').css('color', ''); 
+    }
+  });
 });
+
