d4cdf768d1d95cf5082c22aee7aee91160142847
Jan Edmark Arieta <edarieta09@gmail.com>
2024-02-13 01:26:30 +0000

    adds hover-scrips.js, removes all scripts from client.js into separate js files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -137,20 +137,3 @@ $(document).ready(function () {
   submitAction();
 });
 
-//Adds hovered class when hovering to change favicon color
-$(document).ready(function () {
-  $(document).on('mouseenter', '.favicon i', function () {
-    $(this).addClass('hovered');
-  }).on('mouseleave', '.favicon i', function () {
-    $(this).removeClass('hovered');
-  });
-});
-
-//Adds shadow when hovering over tweets
-$(document).ready(function () {
-  $(document).on('mouseenter', '.tweet', function () {
-    $(this).css('box-shadow', '12px 12px 1px 0px #c5ccf0');
-  }).on('mouseleave', '.tweet', function () {
-    $(this).css('box-shadow', 'none');
-  });
-});
